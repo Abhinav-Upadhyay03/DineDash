bf313499795408c93b9f26b989d9b9d0e96ca825
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2025-02-06 19:54:25 +0530

    price updated

--- src/components/MenuSectionItems.js
+++ src/components/MenuSectionItems.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const MenuSectionItems = (props) => {
     const {item} = props;
-    const{name, price, isVeg, category} = item?.card?.info;
+    const{name, defaultPrice, finalPrice} = item?.card?.info;
     
   return (
     <div className='dish-container'>
     <div className='dish-details'>
        <p>{name}</p> 
-        <p>₹{price/100}</p>
-    
+       {finalPrice ? (
+        <p><span className='old-price'>₹{defaultPrice/100}</span> ₹{finalPrice/100}</p>
+       ):(<p>₹{defaultPrice/100}</p>)}
+        
     </div>
     <button className='dish-add-btn'>ADD</button>
     </div>
